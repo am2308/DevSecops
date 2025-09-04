7d4312357c8c148d8a2f9c0fc84bfc90bf5cfda4
am2308 <b110020510@gmail.com>
2025-09-04 11:00:54 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="background-remover-python-app",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "flask",   # add your real dependencies here
+    ],
+)
